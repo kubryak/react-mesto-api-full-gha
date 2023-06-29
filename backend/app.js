a2673d5415f364ff58c261b4146ee2b62f955a07
kubryak <al.lcn@yandex.ru>
2023-06-29 17:57:47 +0500

    Добавить пакет cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const router = require('./routes');
 const errorHandler = require('./middlewares/error');
@@ -9,6 +10,8 @@ const errorHandler = require('./middlewares/error');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(DB_URL);
 
